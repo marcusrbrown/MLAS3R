1412b0de6d3ed9e60b107b96cc1b89e6604973fa
Marcus R. Brown <mrossbrown@gmail.com>
2015-10-10 20:33:35 -0700

    Add the CoreGameMode C++ class and MLAS3RGameMode Blueprint that derives from it.

---
+++ Source/MLAS3R/CoreGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "CoreGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class MLAS3R_API ACoreGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
