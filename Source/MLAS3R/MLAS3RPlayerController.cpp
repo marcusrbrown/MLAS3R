e686e17d660e5e9a7f34c00c22c82382b41c7d20
Marcus R. Brown <mrossbrown@gmail.com>
2015-10-14 09:51:52 -0700

    Add the MLAS3RPlayerController C++ class.

---
+++ Source/MLAS3R/MLAS3RPlayerController.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MLAS3R.h"
+#include "MLAS3RPlayerController.h"
+
+
+
+
---
+++ Source/MLAS3R/MLAS3RPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "MLAS3RPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MLAS3R_API AMLAS3RPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
