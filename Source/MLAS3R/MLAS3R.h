bd1bf1363720a59aa822034bccf81e228840acdf
Marcus R. Brown <mrossbrown@gmail.com>
2015-12-03 05:21:49 +0000

    Add macros to disable and enable optimization on a function-by-function basis.
    Copied from Perforce
     Change: 236

--- Source/MLAS3R/MLAS3R.h
+++ Source/MLAS3R/MLAS3R.h
@@ -3,3 +3,13 @@
 #pragma once
 
 #include "Engine.h"
+
+// Use these macros to disable and enable optimization on a function-by-function basis. They should be placed
+// immediately before and after each function where optimization is to be disabled and enabled.
+#if defined(__clang__)
+#define MLAS3R_DISABLE_OPTIMIZATION __attribute__((optnone))
+#define MLAS3R_ENABLE_OPTIMIZATION
+#elif defined(_MSC_VER)
+#define MLAS3R_DISABLE_OPTIMIZATION PRAGMA_DISABLE_OPTIMIZATION_ACTUAL
+#define MLAS3R_ENABLE_OPTIMIZATION PRAGMA_ENABLE_OPTIMIZATION_ACTUAL
+#endif
